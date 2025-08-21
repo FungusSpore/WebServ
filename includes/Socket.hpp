4a6f757493e578feb2d9a466cbfccd39b62ca1d6
VeeG14WSL <v.nkz92@gmail.com>
2025-08-22 05:53:32 +0800

    fix: fixes merge conflict with IO, should be good can double check

--- includes/Socket.hpp
+++ includes/Socket.hpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include "MiniHttp.hpp"
 #include "WebServer.hpp"
+#include <ctime>
 
 // class MiniHttp;
 // class Webserver;
@@ -17,8 +18,10 @@ class Epoll;
 /// Forward fd is used for server cgi data forwarding by default will be -1 and port will be left empty
 struct Socket{
 public:
-	int		fd;
-	int		clientFd;
+	int					fd;
+	int					clientFd;
+	bool				is_alive;
+	time_t			last_active;
 	std::string port;
 	std::string read_buffer;
 	std::string write_buffer;
@@ -34,9 +37,10 @@ public:
 	// Socket(int fd, int forward_fd);
 	Socket(int fd, int clientFd, WebServer& server);
 	Socket(const Socket& other);
-	Socket& operator=(const Socket& other);
 	~Socket();
+	Socket& operator=(const Socket& other);
 	bool operator==(const Socket& other) const;
+	bool operator<(const Socket& other) const;
 
 	bool runHttp();
 	bool validateCGI();
